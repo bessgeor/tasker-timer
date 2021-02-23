c2cd92e98f9ad3814a37592b87f71f5d848caa57
bessgeor <bessgeor@gmail.com>
2021-02-23 12:49:06 +0300

    Add mutes command processors

---
+++ TaskerTimer/MessageProcessors/MuteProcessors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskerTimer.MessageProcessors
+{
+	internal sealed class MuteTodayProcessor : MutesAbstractProcessor
+	{
+		protected override string Command => "/mute_today";
+		protected override bool ShouldMute => true;
+		protected override string LogDescription => "Muting today tasks";
+		protected override Func<string, bool, CancellationToken, Task<int>> DbExecutor { get; } = Db.Client.MuteTodayTasksAsync;
+	}
+	internal sealed class UnmuteTodayProcessor : MutesAbstractProcessor
+	{
+		protected override string Command => "/unmute_today";
+		protected override bool ShouldMute => false;
+		protected override string LogDescription => "Unmuting today tasks";
+		protected override Func<string, bool, CancellationToken, Task<int>> DbExecutor { get; } = Db.Client.MuteTodayTasksAsync;
+	}
+	internal sealed class MuteProcessor : MutesAbstractProcessor
+	{
+		protected override string Command => "/mute";
+		protected override bool ShouldMute => true;
+		protected override string LogDescription => "Muting all tasks";
+		protected override Func<string, bool, CancellationToken, Task<int>> DbExecutor { get; } = Db.Client.MuteAllTasksAsync;
+	}
+	internal sealed class UnmuteProcessor : MutesAbstractProcessor
+	{
+		protected override string Command => "/unmute";
+		protected override bool ShouldMute => false;
+		protected override string LogDescription => "Unmuting all tasks";
+		protected override Func<string, bool, CancellationToken, Task<int>> DbExecutor { get; } = Db.Client.MuteAllTasksAsync;
+	}
+}
---
+++ TaskerTimer/MessageProcessors/MutesAbstractProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using TaskerTimer.TgClient.Models;
+
+namespace TaskerTimer.MessageProcessors
+{
+	internal abstract class MutesAbstractProcessor : CommandProcessor
+	{
+		protected abstract bool ShouldMute { get; }
+		protected abstract Func<string, bool, CancellationToken, Task<int>> DbExecutor { get; }
+
+		protected abstract string LogDescription { get; }
+
+		public override async Task ProcessAsync( Message message, ILogger logger, CancellationToken cancellation )
+		{
+			var chatId = message?.chat?.id ?? throw new ArgumentNullException( nameof( message ) );
+			logger.LogInformation( "{0} for chat id {1}", LogDescription, chatId );
+			var response = await DbExecutor(chatId, ShouldMute, cancellation).ConfigureAwait( false );
+			var responseMessage = $"{LogDescription} done for {response} tasks";
+			var m = await Telegram.SendMessageAsync( chatId, isHtml: false, responseMessage, taskId: null, logger, cancellation, message.message_id ).ConfigureAwait( false );
+			logger.LogInformation( $"sent message {m.message_id}" );
+		}
+	}
+}
